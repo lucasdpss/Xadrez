3062b1c042b5b1cc8786fec39f12c0faefe49fc4
Antonio Gabriel Fernandes <antgabrielsf@gmail.com>
2020-05-04 14:47:11 -0300

    Implementado movimento da rainha

--- src/pecas/Rainha.java
+++ src/pecas/Rainha.java
@@ -8,5 +8,57 @@ public class Rainha extends Peca {
 		super(cor, iPos, jPos, t);
 		caractere = (cor == 'B')?'q':'Q';
 	}
+	
+	public boolean mov_valido(int id,int jd) {
+		int diferencaI = (id-iPos>=0)?(id-iPos):(iPos-id);
+		int diferencaJ = (jd-jPos>=0)?(jd-jPos):(jPos-jd);
+		char movimento;
 
+		if(id >= 8 || jd >= 8 || id < 0 || jd < 0) return false;
+		
+		if(diferencaI == 0 && diferencaJ == 0) return false;
+		
+		if(diferencaI != 0) {
+			if(diferencaJ == 0) movimento = 'V'; //movimento vertical
+			else if(diferencaJ == diferencaI) movimento = 'D'; //movimento em diagonal
+			else return false;
+		}
+		else movimento = 'H'; //movimento horizontal
+		
+		switch(movimento) {
+		case 'V':
+			for(int v=1;v < diferencaI;v++)
+				if(t.getPeca(iPos + ((id-iPos)>0?v:-v), jPos) != null)
+					return false;
+			break;
+		case 'H':
+			for(int v=1;v < diferencaI;v++)
+				if(t.getPeca(iPos, jPos + ((jd-jPos)>0?v:-v)) != null)
+					return false;
+			break;
+		case 'D':
+			for(int v=1;v < diferencaI;v++)
+				if(t.getPeca(iPos + ((id-iPos)>0?v:-v), jPos + ((jd-jPos)>0?v:-v)) != null)
+					return false;
+			break;
+		}
+		
+		Peca capturada = t.getPeca(id, jd);
+		if(capturada != null && capturada.getCor() == this.getCor()) return false;
+		
+		return true;
+	}
+
+	public boolean mover(int id, int jd) {
+		if(this.mov_valido(id, jd)) {
+			t.setPeca(id, jd, this);
+			t.setPeca(iPos, jPos, null);
+			iPos = id;
+			jPos = jd;
+
+			return true;
+		}
+		else return false;
+	}
+	
 }
